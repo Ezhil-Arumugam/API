ace020b824c690a2fdfccc7e050c54f4fc32b61a
Ezhil-Arumugam <ezhil.arumugam@accenture.com>
2025-06-18 13:05:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/get-image-url', methods=['GET'])
 def redirect_to_image():
-    image_url = "https://ezhil65.blob.core.windows.net/sample/graph.jpg?sp=racw&st=2025-06-17T07:37:36Z&se=2025-06-19T15:37:36Z&sv=2024-11-04&sr=b&sig=f07XhFpg9v0zNkr5NZ7w%2BMjfxdscJdZjnJ5WQDxofUM%3D"
+    image_url = "https://ezhil65.blob.core.windows.net/sample/graph.jpg?sp=r&st=2025-06-18T06:08:23Z&se=2025-06-25T14:08:23Z&sv=2024-11-04&sr=b&sig=vgLM5DrcJ82FUcvUcnSRFlvj%2F9c8%2F6Av0XhYhHylVDA%3D"
     return redirect(image_url, code=302)
 
 if __name__ == '__main__':
