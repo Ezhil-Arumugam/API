3f6352412bd521ff6ec4ed0c685c9b8a575d2f09
Ezhil-Arumugam <ezhil.arumugam@accenture.com>
2025-06-18 13:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-
 from flask import Flask, redirect
 import os
 
 app = Flask(__name__)
 
-@app.route('/get-image-url', methods=['GET'])
+@app.route('/', methods=['GET']) 
 def redirect_to_image():
     image_url = "https://ezhil65.blob.core.windows.net/sample/graph.jpg?sp=r&st=2025-06-18T06:08:23Z&se=2025-06-25T14:08:23Z&sv=2024-11-04&sr=b&sig=vgLM5DrcJ82FUcvUcnSRFlvj%2F9c8%2F6Av0XhYhHylVDA%3D"
     return redirect(image_url, code=302)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000 locally
+    port = int(os.environ.get("PORT", 10000))  # default Render port fallback
     app.run(host='0.0.0.0', port=port, debug=True)
-
