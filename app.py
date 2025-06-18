1afe778548ad23f0fc65ba102683ed203cad9b30
Ezhil-Arumugam <ezhil.arumugam@accenture.com>
2025-06-18 13:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,6 @@ def redirect_to_image():
     return redirect(image_url, code=302)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)
+
